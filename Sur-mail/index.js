ee96928eeb9d2067a0b2773e10b6db92c71a5dd7
plwsdytlcj <cl2445@cornell.edu>
2018-08-03 15:00:59 +0800

    Initial commit

---
+++ Sur-mail/index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const bodyParser = require("body-parser");
+const keys = require("./config/keys");
+require("./models/User");
+require("./models/Survey");
+require("./services/passport");
+mongoose.connect(keys.mongoURI);
+
+const app = express();
+app.use(bodyParser.json());
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require("./routes/authRoutes")(app);
+require("./routes/billingRoutes")(app);
+require("./routes/surveyRoutes")(app);
+
+if (process.env.NODE_ENV === "production") {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static("client/build"));
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+//client id
+//553252266614-018scqipkrusel9pohd4jdc59nt4ubbj.apps.googleusercontent.com
+//client secret
+//bQVgrOPY1vstU95h3hxI3L_t
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
